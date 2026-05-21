f5e0eb73346c096b6382e43e88dd4a2ac2bb3431
PiotrexOG <118371452+PiotrexOG@users.noreply.github.com>
2026-05-21 22:17:36 +0200

    working oc correlation

--- make_dump.bat
+++ make_dump.bat
@@ -1,13 +1,14 @@
 @echo off
+echo [INFO] Pobieram dump z kontenera Docker...
 
-docker exec -t postgres pg_dump ^
-  -U postgres ^
-  -d stock_sim ^
-  --clean ^
-  --if-exists ^
-  --create ^
-  > dump1.sql
+:: 'postgres' to nazwa kontenera z Twojego pliku docker-compose.yml
+docker exec -e PGPASSWORD=postgres postgres pg_dump -U postgres -d stock_sim -F p -b -v > dump2.sql
 
-echo Backup zapisany do dump.sql
+if %ERRORLEVEL% NEQ 0 (
+    echo [BŁĄD] Nie udało się pobrać dumpa. Upewnij się, że kontener Docker działa!
+    pause
+    exit /b
+)
 
+echo [SUKCES] Gotowe! Plik dump2.sql został zapisany.
 pause
